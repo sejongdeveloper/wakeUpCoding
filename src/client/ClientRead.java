261b1675b4679e8318b1707a75a68708132c7c44
seriously <stint@daum.net>
2019-07-03 16:50:34 +0900

    dd
    dd

---
+++ src/client/ClientRead.java
@@ -0,0 +1,59 @@
+package client;
+
+import java.awt.TextArea;
+import java.io.DataInputStream;
+import java.net.Socket;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JTextArea;
+
+public class ClientRead extends Thread{
+	private Socket s;
+	private DataInputStream dis;
+	private StringTokenizer st;
+	private TextArea chatArea;
+	private JList<String> userList;
+	private Vector<String> uList; 
+	
+	public ClientRead(Socket s,ClientAction ca) {
+		this.s = s;
+		this.chatArea = ca.chatArea;
+		this.userList = ca.userList;
+	}
+	@Override
+	public void run() {
+		// 구현하세요.
+		while(true) {
+			try {
+				String msg = dis.readUTF();
+				
+				applyMsg(msg);
+			} catch (Exception e) {
+			}
+		}
+		
+		
+	}
+	
+	// 서버 90%
+	public void applyMsg(String msg) {
+		//토큰으로 알아서 나누세요. 
+		st = new StringTokenizer(msg, "/");
+		String act = st.nextToken(); // 행동
+		String act2 = st.nextToken();
+		
+		// 구현하세요.
+		if(act.equals("Chatting")) {
+			String message = st.nextToken();
+			chatArea.append(act + " : " + message + "\n");
+			
+		} else if(act.equals("New_User")){
+			uList.add(msg);
+			userList.setListData(uList);
+			
+		}
+	}
+}
---
+++ src/client/join/JoinUI.java
@@ -0,0 +1,49 @@
+package client.join;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class JoinUI extends JFrame{
+	JPanel pTotal, p1, p2, p3, p4;
+	JTextField idField, pwdField, nickField;
+	JButton btnNew;
+	
+	public JoinUI() {
+		
+		pTotal = new JPanel(new GridLayout(4, 0, 10, 10));
+		pTotal.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		p1 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		p1.add(new JLabel("아  이  디"));
+		idField = new JTextField(10);
+		p1.add(idField);
+		pTotal.add(p1);
+		
+		p2 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		p2.add(new JLabel("비밀번호"));
+		pwdField = new JTextField(10);
+		p2.add(pwdField);
+		pTotal.add(p2);
+		
+		p3 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		p3.add(new JLabel("닉  네  임"));
+		nickField = new JTextField(10);
+		p3.add(nickField);
+		pTotal.add(p3);
+		
+		p4 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		p4.add(btnNew = new JButton("회원가입"));
+		pTotal.add(p4);
+
+		add(pTotal);
+		setBounds(150,80,300,250);
+		setVisible(true);
+		
+	}
+}
