56901955fda9ecd11a9edd80f56dcf96653ee04a
SeJongDeveloper <51711799+sejongdeveloper@users.noreply.github.com>
2019-07-03 15:46:53 +0900

    머지 배우기
    ㅇㅇ

--- wakeUpCoding/src/client/ClientRead.java
+++ wakeUpCoding/src/client/ClientRead.java
@@ -1,9 +1,13 @@
 package client;
 
+import java.io.DataInputStream;
 import java.net.Socket;
+import java.util.StringTokenizer;
 
 public class ClientRead extends Thread{
 	private Socket s;
+	private DataInputStream dis;
+	private StringTokenizer st;
 	
 	public ClientRead(Socket s) {
 		this.s = s;
@@ -17,7 +21,17 @@ public class ClientRead extends Thread{
 		
 	}
 	// 서버 90%
-	public void applyMsg() {
+	public void applyMsg(String msg) {
+		//토큰으로 알아서 나누세요. 
+		st = new StringTokenizer(msg, "/");
+		String act = st.nextToken(); // 행동
+		String act2 = st.nextToken();
 		
+		// 구현하세요.
+		if(act.equals("행동1")) {
+		
+		} else if(act.equals("행동2")){
+			
+		}
 	}
 }
