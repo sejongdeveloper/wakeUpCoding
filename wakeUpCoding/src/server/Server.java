c51f2138509e6006a4535ff7b2ad7d02c4dc68cc
choiminwoo1 <50131227+choiminwoo1@users.noreply.github.com>
2019-07-03 17:37:45 +0900

    a

--- wakeUpCoding/src/server/Server.java
+++ wakeUpCoding/src/server/Server.java
@@ -1,15 +1,26 @@
 package server;
 
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Hashtable;
 
 public class Server {
+	
 	private ServerSocket ss;
 	private Hashtable<String, Socket> userHash; // 수정가능한 부분
 	
-	public Server() {
+	
+	
+	
+	
+	public Server() { //연결
+		
+		
 		
 		try {
 			ss = new ServerSocket(7777);
@@ -18,13 +29,17 @@ public class Server {
 				while(true) {
 					try {
 						Socket s = ss.accept();
+					
+						
+						
 						ServerGate sg = new ServerGate(s); //읽는
-						//구현 (userHash put())
+						//구현 (userHash put())						
+						userHash = new Hashtable<String, Socket>();
 						
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
-				}
+				}//while
 			}) ;
 			thread.start();
 			
--- wakeUpCoding/src/server/ServerGate.java
+++ wakeUpCoding/src/server/ServerGate.java
@@ -4,24 +4,27 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Hashtable;
 import java.util.StringTokenizer;
-
 
 public class ServerGate extends Thread {
 	Socket s;
 	DataInputStream dis;
 	DataOutputStream dos;
 	StringTokenizer st;
+
+	private String Nick = "";
+
 	public ServerGate(Socket s) {
-		
+
 	}
-	
+
 	@Override
 	public void run() {
 		try {
-			dis = new DataInputStream(s.getInputStream());			
-		
-			while(true) {
+			dis = new DataInputStream(s.getInputStream());
+
+			while (true) {
 				String msg = dis.readUTF();
 				applyMsg(msg);
 			}
@@ -29,23 +32,46 @@ public class ServerGate extends Thread {
 			e.printStackTrace();
 		}
 	}
-	
+
+	// 시작
 	public void applyMsg(String msg) {
-		//토큰으로 알아서 나누세요. 
+		//토큰으로 알아서 나누세요. 첫글은대문자
 		st = new StringTokenizer(msg, "/");
-		String xxx = st.nextToken(); // 행동
-		String xxx2 = st.nextToken();
+		Hashtable userHash = new Hashtable<String, Socket>();
+		userHash.put(Nick,s);
+		
+		String Chatting = st.nextToken(); // 행동
+		String Chatting2 = st.nextToken();
+		System.out.println("Chatting: " + Chatting );
+		System.out.println("Chatting2   : " + Chatting2);
 		
 		// 구현하세요.
-		if(xxx.equals("행동1")) {
+		if(Chatting.equals("Note")) {
+		st = new StringTokenizer(Chatting2 ,"/");
 		
-		} else if(xxx.equals("행동2")){
+		String User = st.nextToken();
+		String Note = st.nextToken();
+		System.out.println("받는사람  : " + User);
+		System.out.println("보낼 내용  : "  +  Note);
+		
+		for (int i = 0; i < userHash.size(); i++) {
 			
-		}
-	}
-	
+			sendMsg sm = (sendMsg)userHash.elementAt(i);
+			
+			if(sm.nick.equals(User)) {
+				sm.sendM("Note" + Nick +"/"+ Note);
+			}
+			else if(Chatting2.equals("Chatting2")){							
+					sm.sendM("Chatting/" + Nick +"/"+ msg);
+					
+			}		
+		}//if end
+		}//for
+	}// 종료
+
 	// 클라이언트 보내기
 	public void sendMsg(String msg) throws IOException {
+
 		dos = new DataOutputStream(s.getOutputStream());
 		dos.writeUTF(msg);
 	}
