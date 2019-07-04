41e9fcc9e0825d16da7c46ed5d130b30e6ff2d3a
koong17 <50865414+koong17@users.noreply.github.com>
2019-07-04 13:17:55 +0900

    ㅇㅇ

--- wakeUpCoding/src/server/ServerGate.java
+++ wakeUpCoding/src/server/ServerGate.java
@@ -68,7 +68,7 @@ public class ServerGate extends Thread {
 			
 			server.userHash.put(act2, s);
 			System.out.println("닉네임 : " + act2 + "==>" + olds);
-			sendAllMsg(act, act2, null);
+			sendAllMsg(act, act2);
 			/////////방뿌리기//////////
 			server.roomHash.put("proto", new Hashtable<String, Socket>());
 			server.roomHash.get("proto").put(act2, s);
