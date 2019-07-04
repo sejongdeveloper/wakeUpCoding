d47e4dbf1becf0788c209743e64894e9817edd83
koong17 <50865414+koong17@users.noreply.github.com>
2019-07-04 10:09:00 +0900

    little change

--- wakeUpCoding/src/client/ClientAction.java
+++ wakeUpCoding/src/client/ClientAction.java
@@ -24,8 +24,8 @@ public class ClientAction extends ClientUI implements ActionListener{
 			JOptionPane.showMessageDialog(null, "채팅방 생성");
 			c.createRoom();
 		} else if (e.getSource() == btnEnter) {
-			c.sendMsg("Chatting/"+c.nick + chatField.getText().trim());
-			JOptionPane.showMessageDialog(null, "엔터");
+			c.sendMsg("Chatting/"+c.nick +"/"+ chatField.getText().trim());
+			JOptionPane.showMessageDialog(null, "전송");
 			
 		}
 	}
