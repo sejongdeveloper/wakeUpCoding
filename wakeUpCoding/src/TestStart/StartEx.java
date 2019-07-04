4e4d47c0eeb463d5d5c6f91e8a02f85445432797
SeJongDeveloper <sejongdeveloper@gamail.com>
2019-07-04 22:48:57 +0900

    집

---
+++ wakeUpCoding/src/TestStart/StartEx.java
@@ -0,0 +1,13 @@
+package TestStart;
+
+import client.login.Login;
+import server.Server;
+
+public class StartEx {
+	public static void main(String[] args) {
+		new Server();
+		new Login();
+		new Login();
+		new Login();
+	}
+}
