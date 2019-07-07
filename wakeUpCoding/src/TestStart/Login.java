1206345ac8a0e126e4c93d95609dd53925e320c8
SeJongDeveloper <sejongdeveloper@gamail.com>
2019-07-07 21:37:52 +0900

    TestStart 구조조정

---
+++ wakeUpCoding/src/TestStart/Login.java
@@ -0,0 +1,14 @@
+package TestStart;
+
+import client.login.LoginAction;
+
+public class Login {
+	public Login() {
+		new LoginAction();
+	}
+	
+	public static void main(String[] args) {
+		new Login();
+	}
+}
+
